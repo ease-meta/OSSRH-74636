bfeee430db24a6ebe001cdb1742a049fa2b4bd1e
孤冰 <leijian0128@163.com>
2019-11-17 16:00:00 +0800

    结构重构

---
+++ cloud-business-center/cloud-business-admin/src/main/java/com/open/cloud/CloudApplication.java
@@ -0,0 +1,13 @@
+package com.open.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class CloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudApplication.class, args);
+	}
+}
---
+++ cloud-business-center/cloud-business-api/src/main/java/com/open/cloud/business/api/DemoService.java
@@ -0,0 +1,6 @@
+package com.open.cloud.business.api;
+
+public interface DemoService {
+
+	String sayHello(String message);
+}
