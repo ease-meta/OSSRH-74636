a03dc5cf49b0079f9aed0b96d3a197bdac7b4769
孤冰 <leijian0128@163.com>
2019-12-15 16:45:01 +0800

    feat: region和zone区分

--- cloud-eureka/cloud-eureka-web/src/main/java/com/open/cloud/eureka/web/TestRestController.java
+++ cloud-eureka/cloud-eureka-web/src/main/java/com/open/cloud/eureka/web/TestRestController.java
@@ -5,7 +5,6 @@ import com.open.cloud.eureka.web.restTemplate.RestTemplateClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
