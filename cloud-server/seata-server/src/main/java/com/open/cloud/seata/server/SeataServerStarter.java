ccf28a059767f244b814a4e307f9b5bde1f478ed
leijian <leijian0128@163.com>
2020-03-15 18:11:23 +0800

    依賴修正

--- cloud-server/seata-server/src/main/java/com/open/cloud/seata/server/SeataServerStarter.java
+++ cloud-server/seata-server/src/main/java/com/open/cloud/seata/server/SeataServerStarter.java
@@ -1,3 +1,4 @@
+/*
 package com.open.cloud.seata.server;
 
 import io.seata.server.Server;
@@ -6,19 +7,24 @@ import java.io.IOException;
 
 public class SeataServerStarter {
 
-    /**
+    */
+/**
      * The Server.
-     */
+     *//*
+
     static Server server = null;
 
-    /**
+    */
+/**
      * The entry point of application.
      *
      * @param args the input arguments
      * @throws IOException the io exception
-     */
+     *//*
+
     public static void main(String[] args) throws IOException {
         Server.main(args);
     }
 
 }
+*/
