7bf515c896d30c87119bf1aee84313dcbc8bacd4
leijian <leijian0128@163.com>
2020-03-15 18:43:32 +0800

    feat: jetcache

--- cloud-cache/src/test/java/com/open/cloud/cache/MySpringBootApp.java
+++ cloud-cache/src/test/java/com/open/cloud/cache/MySpringBootApp.java
@@ -1,13 +1,9 @@
 package com.open.cloud.cache;
 
-import com.alicp.jetcache.anno.config.EnableCreateCacheAnnotation;
-import com.alicp.jetcache.anno.config.EnableMethodCache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@EnableMethodCache(basePackages = "com.company.mypackage")
-@EnableCreateCacheAnnotation
 public class MySpringBootApp {
 	public static void main(String[] args) {
 		SpringApplication.run(MySpringBootApp.class);
