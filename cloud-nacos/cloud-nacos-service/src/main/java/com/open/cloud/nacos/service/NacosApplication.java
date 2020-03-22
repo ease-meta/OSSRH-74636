e686a5985e32ece861c635074d4856838926279a
leijian <leijian0128@163.com>
2020-03-22 11:11:54 +0800

    feat: DUBBO

--- cloud-nacos/cloud-nacos-service/src/main/java/com/open/cloud/nacos/service/NacosApplication.java
+++ cloud-nacos/cloud-nacos-service/src/main/java/com/open/cloud/nacos/service/NacosApplication.java
@@ -19,6 +19,9 @@ package com.open.cloud.nacos.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableDiscoveryClient(autoRegister = true)
 @SpringBootApplication
@@ -28,11 +31,11 @@ public class NacosApplication {
         SpringApplication.run(NacosApplication.class, args);
     }
 
-   /* @RestController
+   @RestController
     class EchoController {
         @GetMapping(value = "/echo/{string}")
         public String echo(@PathVariable String string) {
             return string;
         }
-    }*/
+    }
 }
