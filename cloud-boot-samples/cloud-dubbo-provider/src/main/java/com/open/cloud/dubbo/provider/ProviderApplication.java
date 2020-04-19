665eb0689fd7f5c071b873a051a062fe062ec6dc
leijian <leijian0128@163.com>
2020-04-19 11:30:54 +0800

    feat: java设计模式

--- cloud-boot-samples/cloud-dubbo-provider/src/main/java/com/open/cloud/dubbo/provider/ProviderApplication.java
+++ cloud-boot-samples/cloud-dubbo-provider/src/main/java/com/open/cloud/dubbo/provider/ProviderApplication.java
@@ -18,8 +18,10 @@ package com.open.cloud.dubbo.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource(value = "classpath:META-INF/spring/*.xml")
 public class ProviderApplication {
 
     public static void main(String[] args) {
