4a82ec36d8db1d1ebf9ca3847fdef02f65e442ca
leijian <leijian0128@163.com>
2020-05-04 22:00:54 +0800

    feat:私仓地址

---
+++ cloud-boot-project/cloud-boot-flyway/src/test/java/com/open/cloud/flyway/test/Application.java
@@ -0,0 +1,12 @@
+package com.open.cloud.flyway.test;
+
+import org.springframework.boot.SpringApplication;
+
+public class Application {
+
+    public static void main(String[] args) {
+        // 启动 Spring Boot 应用
+        SpringApplication.run(Application.class, args);
+    }
+
+}
