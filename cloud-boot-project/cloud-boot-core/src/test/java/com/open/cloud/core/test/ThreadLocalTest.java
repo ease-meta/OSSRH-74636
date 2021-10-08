4a1dbafc675a583ea43880a532a57f66a61583b0
leijian <leijian0128@163.com>
2021-10-08 10:08:51 +0800

    1

---
+++ cloud-boot-project/cloud-boot-core/src/test/java/com/open/cloud/core/test/ThreadLocalTest.java
@@ -0,0 +1,18 @@
+package com.open.cloud.core.test;
+
+public class ThreadLocalTest {
+    private static ThreadLocal tl = new ThreadLocal<>();
+
+    public static void main(String[] args) throws Exception {
+        tl.set(1);
+        System.out.println(String.format("当前线程名称: %s, main方法内获取线程内数据为: %s",
+                Thread.currentThread().getName(), tl.get()));
+        fc();
+        new Thread(ThreadLocalTest::fc).start();
+    }
+
+    private static void fc() {
+        System.out.println(String.format("当前线程名称: %s, fc方法内获取线程内数据为: %s",
+                Thread.currentThread().getName(), tl.get()));
+    }
+}
