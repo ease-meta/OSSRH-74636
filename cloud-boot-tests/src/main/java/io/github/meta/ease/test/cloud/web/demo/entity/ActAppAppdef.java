c334eff4fce4f71e71395711addb07c3eb38df23
leijian <leijian0128@163.com>
2021-11-24 10:31:47 +0800

    docs(README.md): git提交规范

---
+++ cloud-boot-tests/src/main/java/io/github/meta/ease/test/cloud/web/demo/entity/ActAppAppdef.java
@@ -0,0 +1,143 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.meta.ease.test.cloud.web.demo.entity;
+
+import io.github.meta.ease.mybatis.annotation.TableName;
+
+import java.io.Serializable;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author leijian
+ * @since 2021-10-11
+ */
+@TableName("act_app_appdef")
+public class ActAppAppdef implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+
+    private Integer rev;
+
+    private String name;
+
+    private String key;
+
+    private Integer version;
+
+    private String category;
+
+    private String deploymentId;
+
+    private String resourceName;
+
+    private String description;
+
+    private String tenantId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getRev() {
+        return rev;
+    }
+
+    public void setRev(Integer rev) {
+        this.rev = rev;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public Integer getVersion() {
+        return version;
+    }
+
+    public void setVersion(Integer version) {
+        this.version = version;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDeploymentId() {
+        return deploymentId;
+    }
+
+    public void setDeploymentId(String deploymentId) {
+        this.deploymentId = deploymentId;
+    }
+
+    public String getResourceName() {
+        return resourceName;
+    }
+
+    public void setResourceName(String resourceName) {
+        this.resourceName = resourceName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(String tenantId) {
+        this.tenantId = tenantId;
+    }
+
+    @Override
+    public String toString() {
+        return "ActAppAppdef{" + "id=" + id + ", rev=" + rev + ", name=" + name + ", key=" + key
+                + ", version=" + version + ", category=" + category + ", deploymentId="
+                + deploymentId + ", resourceName=" + resourceName + ", description=" + description
+                + ", tenantId=" + tenantId + "}";
+    }
+}
