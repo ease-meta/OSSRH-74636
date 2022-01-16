94ce00ad311396c98e795428624b7120b48d2614
leijian <leijian0128@163.com>
2022-01-16 15:44:40 +0800

    fix: nacos server-addr

--- cloud-alibaba-examples/nacos-discovery-provider-example/src/main/java/io/github/meta/ease/alibaba/provider/examples/ProviderAlibabaApplication.java
+++ cloud-alibaba-examples/nacos-discovery-provider-example/src/main/java/io/github/meta/ease/alibaba/provider/examples/ProviderAlibabaApplication.java
@@ -3,12 +3,15 @@ package io.github.meta.ease.alibaba.provider.examples;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@EnableFeignClients
 public class ProviderAlibabaApplication {
 
     public static void main(String[] args) {
+        System.setProperty("spring.application.name", "alibaba-provider-examples");
         SpringApplication.run(ProviderAlibabaApplication.class, args);
     }
 }
---
+++ cloud-alibaba-examples/nacos-discovery-provider-example/src/test/java/io/github/meta/ease/alibaba/provider/examples/ProviderAlibabaApplicationTest.java
@@ -0,0 +1,17 @@
+package io.github.meta.ease.alibaba.provider.examples;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * @author leijian
+ * @version 1.0
+ * @date 2022/1/15 14:17
+ */
+public class ProviderAlibabaApplicationTest {
+
+    public static void main(String[] args) throws URISyntaxException {
+        String host = new URI("http://127.0.0.1").getHost();
+
+    }
+}
